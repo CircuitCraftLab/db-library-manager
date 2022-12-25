f747a87f85948e7c47d1cb7eb842cf6d5cc19fb5
acidicMercury8 <acidicmercury8@outlook.com>
2022-12-25 17:48:48 +0300

    Switch to MVVM architecture

---
+++ src/CircuitCraftLab.DbLibraryManager/Views/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+using System.Windows;
+
+namespace CircuitCraftLab.DbLibraryManager.Views;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() => InitializeComponent();
+}
