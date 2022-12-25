14b47d65843d82fc5a7233732bcdab42f3781101
Renat Shakirov <acidicmercury8@outlook.com>
2022-12-25 17:40:03 +0300

    Redefine entry point to use hosting and dependency injection

--- src/CircuitCraftLab.DbLibraryManager/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace CircuitCraftLab.DbLibraryManager;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application {
-}
